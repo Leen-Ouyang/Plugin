ffc3a24736ce486f4097978cbfe3ad0b21b7affc
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-12 00:06:55 +0800

    Update help.lua

--- program/help.lua
+++ program/help.lua
@@ -1,11 +1,11 @@
 help = "help"
 config = {
     msg = {
-        mainmenu="╔═══════════════╗\n            指令档案\n  ——————————\n      输入指令查看详细\n  ——————————\n   指令     效果\n 「info」打开个人档案\n 「mod」 书写个人简介\n 「vnt」 打开事件档案\n 「chat」打开表白墙\n 「cred」打开学分档案\n 「add」  查看新解锁内容\n 「achi」打开成就档案\n 「pnt」  打开积分档案\n 「shop」打开远程商店\n 「bag」  打开远程背包\n╚═══════════════╝\ntips:输入「特殊指令」\n         可解锁某些成就"
+        mainmenu="╔═══════════════╗\n            指令档案\n  ——————————\n      输入指令查看详细\n  ——————————\n   指令     效果\n 「info」打开个人档案\n 「mod」 书写个人简介\n 「chat」社交\n 「cred」打开学分档案\n 「add」  查看新解锁内容\n 「achi」打开成就档案\n 「pnt」  打开积分档案\n 「shop」打开远程商店\n 「bag」  打开远程背包\n╚═══════════════╝\ntips:输入「特殊指令」\n         可解锁某些成就"
     }
 }
 msg_order = {}
-msg_order[help] = "menu"
-function menu()
+msg_order[help] = "Menu"
+function Menu()
     return config.msg.mainmenu
 end
