1653c09db1b72c6482146b49b287c4dc2e90dbc8
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-14 03:42:47 +0800

    Create bag.lua

---
+++ program/bag.lua
@@ -0,0 +1,27 @@
+bag="bag"
+config={
+    msg={
+        item="╔═══════════════╗\n            远程背包\n  ——————————\n   积分 {point}       道具 {num}\n  ——————————\n  {items}\n   指令:\n      查看+道具名\n       使用+道具名\n╚═══════════════╝"
+    }
+}
+msg_order={}
+msg_order[bag]="viewBag"
+
+msg_order[info] = "getInformation"
+player_information="player.json"
+data = getSelfData(player_information)
+players = data:get(nil, {})
+if(players == nil)then
+    players = {}
+end
+
+function viewBag(msg)
+    local QQ=tostring(msg.fromQQ)
+    local point = players[QQ]["points"]
+    local items = players[QQ]["Bag"]["item"]
+    local num = players[QQ]["Bag"]["sum"]
+    config.msg.item = config.msg.sign_success:gsub("{point}", point)
+    config.msg.item = config.msg.sign_success:gsub("{num}", num)
+    config.msg.item = config.msg.sign_success:gsub("{items}", items)
+    return config.msg.item
+end
