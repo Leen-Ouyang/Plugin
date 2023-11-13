cba03fb75ac559896f5529ed66ed66e8d7574629
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-14 02:45:29 +0800

    Update sign.lua

--- program/sign.lua
+++ program/sign.lua
@@ -17,6 +17,7 @@ function sign(msg)
     QQ=tostring(msg.fromQQ)
     players[QQ]["Count"]["sign"]=players[QQ]["Count"]["sign"]+1
     players[QQ]["points"]=players[QQ]["points"]+10+math.random(1, 10)
+    data:set(players)
     config.msg.sign_success = config.msg.sign_success:gsub("{point}", point)
     return config.msg.sign_success
 end
