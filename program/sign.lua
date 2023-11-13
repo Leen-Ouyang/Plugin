dff547ab35ac2637582a80528883dd01026aab8c
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-14 02:27:17 +0800

    Create sign.lua

---
+++ program/sign.lua
@@ -0,0 +1,22 @@
+s="sign"
+ocnfig={
+    msg={
+        sign_success="签到成功！积分+{point}"
+    }
+}
+msg_order={}
+msg_order[s]="sign"
+
+player_information="player.json"
+data = getSelfData(player_information)
+players = data:get(nil, {})
+if(players == nil)then
+    players = {}
+end
+function sign(msg)
+    QQ=tostring(msg.fromQQ)
+    players[QQ]["Count"]["sign"]=players[QQ]["Count"]["sign"]+1
+    players[QQ]["points"]=players[QQ]["points"]+10+math.random(1, 10)
+    config.msg.sign_success = config.msg.sign_success:gsub("{point}", point)
+    return config.msg.sign_success
+end
