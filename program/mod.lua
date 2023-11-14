7b37810d994e69a6c00dce60aabcd609c53ba48b
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-14 13:15:23 +0800

    Create mod.lua

---
+++ program/mod.lua
@@ -0,0 +1,23 @@
+mod="mod"
+config={
+    msg={
+        update_intro="个人简介保存成功！"
+    }
+}
+order_msg={}
+order_msg[mod]="updateIntro"
+
+player_information="player.json"
+data = getSelfData(player_information)
+players = data:get(nil, {})
+if(players == nil)then
+    players = {}
+end
+
+function updateIntro(msg)
+    local introduction = string.match(msg.fromMsg,"[%s]*(.-)[%s]*$",#mod+1)
+    local QQ=tostring(msg.fromQQ)
+    players[QQ]["Info"]["Introduction"]=introduction
+    data:set(players)
+    return config.msg.update_intro
+end
