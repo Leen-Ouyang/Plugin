13cf9d82ac151997abef872a028f5e697992e018
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-16 22:54:28 +0800

    Update mod.lua

--- program/mod.lua
+++ program/mod.lua
@@ -7,16 +7,15 @@ config={
 order_msg={}
 order_msg[mod]="updateIntro"
 
-player_information="player.json"
-data = getSelfData(player_information)
-players = data:get(nil, {})
-if(players == nil)then
-    players = {}
-end
-
 function updateIntro(msg)
     local introduction = string.match(msg.fromMsg,"[%s]*(.-)[%s]*$",#mod+1)
     local QQ=tostring(msg.fromQQ)
+    player_information="player.json"
+    data = getSelfData(player_information)
+    players = data:get(nil, {})
+    if(players == nil)then
+        players = {}
+    end
     if (players[QQ]["Info"]["Nickname"]==nil) then 
         return "未创建角色，请先创建角色「创建新角色」"
     end
