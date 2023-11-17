3b84c0dda11ba03577cff56746778986f9edb965
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-18 01:06:14 +0800

    Update achi.lua

--- program/achi.lua
+++ program/achi.lua
@@ -4,7 +4,6 @@ lock_achi="未解锁"
 config={
     msg={
         achievementmenu="╔════════════════════╗\n                   成就档案\n   —————————————\n              输入指令查看详细\n   —————————————\n   指令及效果：\n    「已解锁」 查看已解锁成就\n    「未解锁」 查看未解锁成就\n╚════════════════════╝\ntips:触发极低概率特殊事件可解锁相应成就",
-        my_achievement="",
         lock_achievement=""
     }
 }
@@ -42,10 +41,34 @@ function viewMyAchievement(msg)
     for i=1,#player_achi do
         local achi_id=player_achi[i]
         local name=achievements[achi_id]["name"]
-        namebuff=namebuff.."\n".."        "..name.."    "
+        namebuff=namebuff.."\n".."        "..name.."\n".."            "
         local buffs=achievements[achi_id]["buff"]
-        for key,val in pairs(buffs) do
-            local buff=key..":"..val
+        for key,val in pairs(buffs) do 
+            local buff_name=" "
+            if key=="point" then
+                buff_name="积分"
+            elseif key=="INT" then
+                buff_name="智力"
+            elseif key=="CON" then
+                buff_name="体质"
+            elseif key=="WIL" then
+                buff_name="意志"
+            elseif key=="LUC" then
+                buff_name="运气"
+            elseif key=="energy" then
+                buff_name="精力"
+            elseif key=="mood" then
+                buff_name="心情"
+            elseif key=="credit" then
+                buff_name="学分"
+            end 
+            local sym=string.sub(val,1,1)
+            local buff=" "
+            if (sym=="-") then
+                buff=buff_name..val
+            else
+                buff=buff_name.."+"..val
+            end
             namebuff=namebuff..buff.."  "
         end
     end
