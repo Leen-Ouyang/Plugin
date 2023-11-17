2154c949306249f02c80ba1f622f532ebdc07dfd
102299222 <144311334+102299222@users.noreply.github.com>
2023-11-17 23:34:21 +0800

    Update achi.lua

--- program/achi.lua
+++ program/achi.lua
@@ -3,7 +3,7 @@ my_achi="已解锁"
 lock_achi="未解锁"
 config={
     msg={
-        achievementmenu="╔════════════════════╗\n           成就档案\n   —————————————\n      输入指令查看详细\n   —————————————\n   指令及效果：\n    「已解锁」 查看已解锁成就\n    「未解锁」 查看未解锁成就\n╚════════════════════╝\ntips:触发极低概率特殊事件可解锁相应成就",
+        achievementmenu="╔════════════════════╗\n                   成就档案\n   —————————————\n              输入指令查看详细\n   —————————————\n   指令及效果：\n    「已解锁」 查看已解锁成就\n    「未解锁」 查看未解锁成就\n╚════════════════════╝\ntips:触发极低概率特殊事件可解锁相应成就",
         my_achievement="",
         lock_achievement=""
     }
@@ -37,16 +37,24 @@ function viewMyAchievement(msg)
         return "未创建角色，请先创建角色「创建新角色」"
     end
     local player_achi=players[QQ]["Achievement"]
-    local achi_id = {}
-    for val in mainatt:gmatch("%S+") do
-        table.insert(achi_id, val)
+    local achi_namebuff={}
+    local namebuff="已解锁成就："
+    for i=1,#player_achi do
+        local achi_id=player_achi[i]
+        local name=achievements[achi_id]["name"]
+        namebuff=namebuff.."\n".."        "..name.."    "
+        local buffs=achievements[achi_id]["buff"]
+        for key,val in pairs(buffs) do
+            local buff=key..":"..val
+            namebuff=namebuff..buff.."  "
+        end
     end
-    return config.msg.my_achievement
+    return namebuff
 end
 
 function viewLockAchievement(msg)
     local QQ=tostring(msg.fromQQ)
-    if (players[QQ]==nil) then 
+    if (players[QQ]["Info"]["Nickname"]==nil) then 
         return "未创建角色，请先创建角色「创建新角色」"
     end
     return config.msg.lock_achievement
